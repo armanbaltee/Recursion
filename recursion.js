fafbe9c698eb21ebbcc8bc2920da32aef3360bf0
armanbaltee <armanalibaltee@gmail.com>
2025-09-05 23:02:48 +0500

    first commit

---
+++ recursion.js
@@ -0,0 +1,21 @@
+// Problem 1
+// sum of array num
+
+let arr = [1,2,3,4,5];
+
+// function sumArrayValue(arr){
+//     if(arr.length==0){
+//         return 0
+//     }
+//     return arr[0] + sumArrayValue(arr.slice(1));
+// }
+function sumArrayValue(arr){
+    if(arr.length==0){
+        return 0
+    }
+    let firstIndexVal = arr.shift();
+    return firstIndexVal + sumArrayValue(arr);
+}
+
+let total = sumArrayValue(arr)
+console.log("total---->", total)
